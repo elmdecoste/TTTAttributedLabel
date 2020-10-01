cb45fe0a089fa373d99dfb953bb92dbb618d0bf6
DeCoste, Liam <ldecoste@csid.com>
2020-10-01 13:24:51 -0500

    Added Swift Package support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TTTAttributedLabel",
+    products: [
+        .library(name: "TTTAttributedLabel", targets: ["TTTAttributedLabel"])
+    ],
+    targets: [
+        .target(
+            name: "TTTAttributedLabel",
+            path: "TTTAttributedLabel",
+            publicHeadersPath: "",
+            cSettings: [
+                .headerSearchPath("")
+            ]
+        )
+    ]
+)
